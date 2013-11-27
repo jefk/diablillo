20f6cfb3c870228d79f4c5a479131b397c62952e
jefk <speedracer111@gmail.com>
2013-11-27 11:38:52 -0800

    rename the class

---
+++ lib/gusto.rb
@@ -0,0 +1,28 @@
+class Object
+  alias_method :old_method_missing, :method_missing
+
+  def method_missing(method, *args, &block)
+    return old_method_missing(method, *args, &block) unless __¿calificado?(method)
+
+    gringo = method.to_s.gsub(/^[¿¡]/, '').to_sym
+    if respond_to? gringo
+      send(gringo, *args, &block)
+    else
+      old_method_missing(method, *args, &block)
+    end
+  end
+
+  private
+
+  def __¿calificado?(method)
+    __inversions[ method[0] ] == method[-1]
+  end
+
+  def __inversions
+    {
+      '¿' => '?',
+      '¡' => '!',
+    }
+  end
+
+end
--- spec/gusto_spec.rb
+++ spec/gusto_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-class Que
+class Gusto
   def que_va?
     true
   end
@@ -8,16 +8,23 @@ class Que
   def fiesta!
     'party time'
   end
+
+  def ¿y_tú?
+    5
+  end
 end
 
-describe Que do
-  let(:instance) { Que.new }
+describe Gusto do
+  let(:instance) { Gusto.new }
 
   it 'leaves normal method calling alone' do
     expect{ instance.que_va? }.not_to raise_error Exception
     expect{ instance.fiesta! }.not_to raise_error Exception
+    expect{ instance.¿y_tú? }.not_to raise_error Exception
     expect{ instance.nada }.to raise_error NoMethodError
+
     expect{ instance.¿interobang! }.to raise_error NoMethodError
+    expect{ instance.¿fiesta! }.to raise_error NoMethodError
   end
 
   it 'responds to methods especialles' do
